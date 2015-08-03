ab9618768c9e775e26ad25025ce66a0abbbb83e0
Sam L Martinez & Steve Smietana <student@epicodus.com>
2015-08-03 14:35:19 -0700

    Pushing files up

--- car.php
+++ car.php
@@ -13,6 +13,38 @@ class Car
       $this->miles = $miles;
       $this->photo = $photo;
     }
+    function setModel($new_make_model)
+    {
+        $this->make_model = (string) $new_make_model;
+    }
+    function getModel()
+    {
+        return $this->make_model;
+    }
+    function setPrice($new_price)
+    {
+        $this->price = (float) $new_price;
+    }
+    function getPrice()
+    {
+        return $this->price;
+    }
+    function setMiles($new_miles)
+    {
+        $this->miles = (float) $new_miles;
+    }
+    function getMiles()
+    {
+        return $this->miles;
+    }
+    function setPhoto($new_photo)
+    {
+        $this->photo = (string) $new_photo;
+    }
+    function getPhoto()
+    {
+        return $this->photo;
+    }
 }
 
 $first_car = new Car("2014 Porsche 911", 114991, 7864, "images/porsche.jpg");
@@ -24,8 +56,10 @@ $cars = array($first_car, $second_car, $third_car, $fourth_car);
 
 $cars_matching_search = array();
 foreach ($cars as $car) {
-    if ($car->price < $_GET["price"]) {
+    if ($car->getPrice() < $_GET["price"]) {
+      if($car->getMiles() < $_GET["miles"]) {
         array_push($cars_matching_search, $car);
+      }
     }
 }
 
@@ -34,19 +68,27 @@ foreach ($cars as $car) {
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet>"
     <title>Your Car Dealership's Homepage</title>
 </head>
 <body>
-    <h1>Your Car Dealership</h1>
+    <h1>Steve and Sam's Hotrod Dealership</h1>
     <ul>
         <?php
             foreach ($cars_matching_search as $car) {
-                echo "<li> $car->make_model </li>";
+                $car_make = $car->getModel();
+                $car_price = $car->getPrice();
+                $car_miles = $car->getMiles();
+                $car_photo = $car->getPhoto();
+                echo "<li> $car_make</li>";
                 echo "<ul>";
-                    echo "<li> $car->price </li>";
-                    echo "<li> Miles: $car->miles </li>";
-                    echo "<li> <img src= '$car->photo'> </li>";
+                    echo "<li> $car_price </li>";
+                    echo "<li> Miles: $car_miles </li>";
+                    echo "<li> <img src= '$car_photo'> </li>";
                 echo "</ul>";
+            }
+            if(empty($cars_matching_search)) {
+              echo "No cars!";
             }
         ?>
     </ul>
